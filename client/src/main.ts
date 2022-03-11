b89cbd9b9ecdf3cac5cbe3651f71fe1f551c29f7
Adithya Parimi <parimichowdary1998@gmail.com>
2022-03-10 22:51:34 -0500

    Included Pinia and router in main.ts

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import { createPinia } from 'pinia';
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import router from './router'
+
+createApp(App)
+    .use(router)
+    .use(createPinia())
+    .mount('#app')
