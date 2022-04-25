693bcd5be32ab54a289c81ca187032cfe798f7bf
AdithyaParimi <parimichowdary1998@gmail.com>
2022-04-25 15:33:24 -0400

    Revert "Revert last commit"
    This reverts commit b64e76683ab3338c2de7f36a5a10b4dbe708addd.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,47 +1,23 @@
-<script setup>
-import Form from "./pages/Form.vue";
-import Login from "./pages/Login.vue";
-import CreateTask from "./pages/CreateTask.vue";
-import "bootstrap/dist/css/bootstrap.css";
+<script setup la
+  components: { Nav },ng="ts">
+
+
+import Nav from './components/Nav.vue'
+
+
 </script>
 
 <template>
-  <!-- <Form /> -->
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <!-- <router-link class="margin-right" to="/">Login</router-link>
-        <router-link class="margin-right" to="/createTask"
-          >Create Task</router-link
-        > -->
-      </div>
-    </div>
-  </div>
-  <!-- <router-view></router-view> -->
+  <Nav />
 
-  <div v-if="loginStatus">
-    <Login @lcallback="updateLogin" />
+<div class="container">
+       <router-view />
   </div>
-  <div v-if="!loginStatus">
-    <CreateTask />
-  </div>
+
+  
+
+
 </template>
-<script>
-export default {
-  data() {
-    return {
-      loginStatus: true,
-    };
-  },
-  methods: {
-    updateLogin: function () {
-      this.loginStatus = false;
-    },
-  },
-};
-</script>
-<style scoped>
-.margin-right {
-  margin-right: 20px;
-}
+
+<style>
 </style>
--- client/src/pages/Login.vue
+++ client/src/pages/Login.vue
@@ -1,129 +1,44 @@
 <script setup lang="ts">
-    emailLogin: "";
-    passwordLogin: "";
-    function doLogin(e) {
-      alert(this.emailLogin + " " + this.passwordLogin);
-      this.$emit("lcallback");
-    // method to format as currency
-  }
+import { ref } from "@vue/reactivity"
+import { Login } from "../models/session"
+
+const username= ref();
+const password= ref();
+
+function login()
+{
+    Login(username.value, password.value)
+}
 </script>
 
 <template>
-  <div id="main">
-    <form v-cloak>
-      <h1>Login</h1>
-      <input
-        v-model="emailLogin"
-        type="email"
-        class="form-control"
-        placeholder="Email"
-        required
-      />
-      <input
-        v-model="passwordLogin"
-        type="password"
-        class="form-control"
-        placeholder="Password"
-        required
-      />
-      <input type="submit" class="btn btn-primary" @click="doLogin" />
-    </form>
-  </div>
+    <div class="section">
+         <h1 class="title">Login</h1>
+        <div>
+            <label for="username">User Name</label>
+            <input id="username" class="input" placeholder="User Name " v-model="username" />
+        </div>
+        <div>
+            <label for="password">Password</label>
+            <input id="password" class="input" placeholder="Password" v-model="password" />
+        </div>
+        <button class="button is-primary my-5" @click="login"> 
+            <span class="icon">
+               <i class="fa fa-sign-in"></i>
+            </span>
+            <span>Login</span>
+        </button>
+        &nbsp;
+        <router-link class="button is-warning my-5" to="/signup">
+            <span class="icon">
+                <i class="fa fa-sign-in"></i>
+            </span>
+            <span>Signup</span>
+        </router-link>
+    </div>
 </template>
 
 
 <style scoped>
-@import url(https://fonts.googleapis.com/css2?family=Montserrat:wght@700&family=Shadows+Into+Light);
-[v-cloak] {
-  display: none;
-}
 
-* {
-  margin: 0;
-  padding: 0;
-}
-
-body {
-  font: 15px/1.3 "Open Sans", sans-serif;
-  color: #5e5b64;
-  text-align: center;
-}
-
-a,
-a:visited {
-  outline: none;
-  color: #389dc1;
-}
-
-a:hover {
-  text-decoration: none;
-}
-
-section,
-footer,
-header,
-aside,
-nav {
-  display: block;
-}
-
-/*-------------------------
-    The order form
---------------------------*/
-
-form {
-  background-color: #8931ef;
-  border-radius: 2px;
-  box-shadow: 0 1px 1px #ccc;
-  width: 400px;
-  padding: 35px 60px;
-  margin: 50px auto;
-}
-
-form h1 {
-  color: #fff;
-  font-size: 64px;
-  font-family: "Cookie", cursive;
-  font-weight: normal;
-  line-height: 1;
-  text-shadow: 0 3px 0 rgba(0, 0, 0, 0.1);
-}
-
-form ul {
-  list-style: none;
-  color: #fff;
-  font-size: 20px;
-  font-weight: bold;
-  text-align: left;
-  margin: 20px 0 15px;
-}
-
-form ul li {
-  padding: 20px 30px;
-  background-color: #a5949a;
-  margin-bottom: 8px;
-  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
-  cursor: pointer;
-}
-
-form ul li span {
-  float: right;
-}
-
-form ul li.active {
-  background-color: #87e911;
-}
-
-div.total {
-  border-top: 1px solid rgba(255, 255, 255, 0.5);
-  padding: 15px 30px;
-  font-size: 20px;
-  font-weight: bold;
-  text-align: left;
-  color: #fff;
-}
-
-div.total span {
-  float: right;
-}
 </style>
