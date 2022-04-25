1f95ee664772d47e0f15c8ffa2da7fe4456ebe95
AdithyaParimi <parimichowdary1998@gmail.com>
2022-04-25 18:07:08 -0400

    Added getters

--- client/src/models/task.ts
+++ client/src/models/task.ts
@@ -6,20 +6,31 @@ export const usetasks = defineStore('tasks', {
 
     state: () => ({
         tasks: [
-            { id:0, assignedTo: 1, isCompleted: false, message: 'Make Bulma great again', dueDate: new Date("07-05-2021"), isOwned: 2 },
-            { id:1, assignedTo: 2, isCompleted: true, message: 'Add some more features' , dueDate: new Date("07-26-2021"), isOwned: 2 },
-            { id:2, assignedTo: 3, isCompleted: false, message: 'Make a github account' , dueDate: new Date("08-23-2021"), isOwned: 3 },
-            { id:3, assignedTo: 2, isCompleted: true, message: 'Learn how to use github' , dueDate: new Date("09-10-2021"),isOwned: 3},
-            { id:4, assignedTo: 3, isCompleted: false, message: 'add a .gitignore file' , dueDate: new Date("07-10-2022"), isOwned: 1 },
+            { id:0, assignedTo: 1, completed: true, message: 'Make Bulma great again', dueDate: new Date("12-01-2022"), createdBy: 2 },
+            { id:1, assignedTo: 2, completed: true, message: 'Add some more features' , dueDate: new Date("10-30-2022"), createdBy: 2 },
+            { id:2, assignedTo: 3, completed: false, message: 'Make a github account' , dueDate: new Date("1-27-2021"), createdBy: 3 },
+            { id:3, assignedTo: 2, completed: true, message: 'Learn how to use github' , dueDate: new Date("2-11-2021"),createdBy: 3},
+            { id:4, assignedTo: 3, completed: false, message: 'add a .gitignore file' , dueDate: new Date("08-09-2023"), createdBy: 1 },
         ] 
     }),
+    getters:{
+        completedTasks(): any{
+            return this.tasks.filter(t=>t.completed);
+        },
+
+        currentTasks(): any{
+            return this.tasks.filter(t=>!t.completed);
+        }
+    },
     actions: {
         close(index: number) {
             this.tasks.splice(index, 1);
         },
 
-        createTasks(id: any, message: any, dueDate: any, assignedTo: any, userId: any , isCompleted: any= false){
-            this.tasks.push({id:id, isCompleted: isCompleted,message: message, dueDate: new Date(dueDate),isOwned: userId,assignedTo: assignedTo});
-        }
+        addNewTask(id: any, message: any, dueDate: any, assignedTo: any, userId: any , completed: any= false){
+            this.tasks.unshift({id:id, completed: completed,message: message, dueDate: new Date(dueDate),createdBy: userId,assignedTo: assignedTo});
+        },
+
+        
     }
 })
