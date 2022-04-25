b64e76683ab3338c2de7f36a5a10b4dbe708addd
AdithyaParimi <parimichowdary1998@gmail.com>
2022-04-25 15:25:39 -0400

    Revert last commit

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,47 @@
-<script setup la
-  components: { Nav },ng="ts">
-
-
-import Nav from './components/Nav.vue'
-
-
+<script setup>
+import Form from "./pages/Form.vue";
+import Login from "./pages/Login.vue";
+import CreateTask from "./pages/CreateTask.vue";
+import "bootstrap/dist/css/bootstrap.css";
 </script>
 
 <template>
-  <Nav />
-
-<div class="container">
-       <router-view />
+  <!-- <Form /> -->
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <!-- <router-link class="margin-right" to="/">Login</router-link>
+        <router-link class="margin-right" to="/createTask"
+          >Create Task</router-link
+        > -->
+      </div>
+    </div>
   </div>
+  <!-- <router-view></router-view> -->
 
-  
-
-
+  <div v-if="loginStatus">
+    <Login @lcallback="updateLogin" />
+  </div>
+  <div v-if="!loginStatus">
+    <CreateTask />
+  </div>
 </template>
-
-<style>
+<script>
+export default {
+  data() {
+    return {
+      loginStatus: true,
+    };
+  },
+  methods: {
+    updateLogin: function () {
+      this.loginStatus = false;
+    },
+  },
+};
+</script>
+<style scoped>
+.margin-right {
+  margin-right: 20px;
+}
 </style>
