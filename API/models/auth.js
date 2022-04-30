42dbef52edf5009b9418c640a51e994b2a213f69
AdithyaParimi <parimichowdary1998@gmail.com>
2022-04-30 02:00:24 -0400

    Cleaned up auth guard

--- API/models/auth.js
+++ API/models/auth.js
@@ -1,31 +1,3 @@
-// const requireAuth = (req, res, next) => {
-//   let token;
-
-//   if (
-//     req.headers.authorization &&
-//     req.headers.authorization.startsWith("Bearer ")
-//   ) {
-//     try {
-//       // Get token from the header
-//       token = req.headers.authorization.split(" ")[1];
-//       // Verify the token
-//       const decoded = jwt.verify(token, "abcd1234");
-//       next();
-//     } catch (err) {
-//       console.log(err);
-//       res.status(401);
-//       throw new Error("Not authorized, Invalid Token!");
-//     }
-//   }
-//   if (!token) {
-//     res.status(401);
-//     throw new Error("Not authorized, No Token!");
-//   }
-// };
-
-// module.exports = {
-//   requireAuth,
-// };
 
 module.exports = {
   requireAuth(req, res, next) {
