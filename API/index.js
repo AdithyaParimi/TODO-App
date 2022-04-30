d339752f34dde2294427d9239722907772bf50a2
AdithyaParimi <parimichowdary1998@gmail.com>
2022-04-30 01:57:57 -0400

    Made neccessary changes to index.js file

--- API/index.js
+++ API/index.js
@@ -1,14 +1,35 @@
-const http = require('http');
+require('dotenv').config();
+const express = require("express");
+const userRoutes = require("./controllers/user");
+const taskRoutes = require("./controllers/task");
+const userModel = require('./models/user');
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req,res)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-});
-
-server.listen(port, hostname, ()=>{
-    console.log(`Server is running at http://${hostname}:${port}`);
-});
+const app = express();
+const port = process.env.PORT || 3005;
+app.use(express.json())
+    .use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        next();
+    })
+    .use((req, res, next) => {
+        const auth = req.headers.authorization;
+        if (auth) {
+            const token = auth.split(' ')[1];
+            userModel.fromToken(token)
+                .then(user => {
+                    req.user = user;
+                    next();
+                }).catch(next);
+        } else {
+            next();
+        }
+    })
+    .use("/api/tasks/", taskRoutes)
+    .use("/api/users/", userRoutes)
+    .use((err, req, res, next) => {
+        console.error(err);
+        res .status(err.statusCode || 500)
+            .send({ errors: [ err.message ?? 'Internal server error' ] });
+    })
+app.listen(port, () => console.log(`Server running on port ${port}`));
