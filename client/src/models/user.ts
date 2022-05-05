4a2d5373fd626e0dbaaae019145d6e06163d9d45
AdithyaParimi <parimichowdary1998@gmail.com>
2022-05-05 15:10:02 -0400

    removed static users list

--- client/src/models/user.ts
+++ client/src/models/user.ts
@@ -9,29 +9,3 @@ export interface User {
     token?: string;
 }
 
-export const list: User[] = [
-    {
-        name: 'Adithya',
-        handle: 'Adhi',
-        password: 'password',
-        email: 'parimia@newpaltz.edu',
-        pic: 'https://randomuser.me/api/portraits/men/1.jpg',
-        id: 1,
-    },
-    {
-        name: 'Sasi',
-        handle: 'sasi',
-        password: 'password',
-        email: 'maddinenis2@newpaltz.com',
-        pic: 'https://randomuser.me/api/portraits/men/2.jpg',
-        id: 2,
-    },
-    {
-        name: 'Ramu',
-        handle: 'ram',
-        password: 'password',
-        email: 'malempar2@newpaltz.edu',
-        pic: 'https://randomuser.me/api/portraits/women/3.jpg',
-        id: 3,
-    },
-];
